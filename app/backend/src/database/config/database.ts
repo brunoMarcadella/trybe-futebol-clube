af814526b06be6ee938a08e09707f11a41dddf33
Bruno Marcadella <bruno.marcadella@hotmail.com>
2024-10-22 11:59:26 -0300

    Alterando config do backend

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -1,16 +1,16 @@
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456',
-  database: 'TRYBE_FUTEBOL_CLUBE',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3306,
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  host: process.env.MYSQLHOST,
+  port: Number(process.env.MYSQLPORT),
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
   },
   logging: false,
-}
+};
 
 export = config;
